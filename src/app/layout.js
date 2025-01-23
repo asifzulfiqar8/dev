8fa8669707f15611da7dfbde50d783784fcf53ce
farhanrasheed1121 <farhanrasheed1121@gmail.com>
2025-01-23 19:23:50 +0500

    pages created for home page

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,31 +17,11 @@ export const metadata = {
   github: {
     profile: "https://github.com/asifzulfiqar8",
   },
-  // icons: [
-  //   {
-  //     rel: "icon",
-  //     type: "image/png",
-  //     href: "assets/images/asif.png",
-  //   },
-  // ],
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content={metadata.description} />
-        <meta property="og:title" content={metadata.openGraph.title} />
-        <meta property="og:description" content={metadata.openGraph.description} />
-        <meta property="og:url" content={metadata.openGraph.url} />
-        <meta property="og:image" content={metadata.openGraph.image} />
-        <link rel="icon" href={metadata.icons[0].href} />
-        <title>{metadata.title}</title>
-        <meta name="linkedin:profile" content={metadata.linkedin.profile} />
-        <meta name="github:profile" content={metadata.github.profile} />
-      </head> */}
       <body
         className={`antialiased`}
       >
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,17 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
+import SkillsScroller from "@/components/SkillsScroller";
 
 export default function Home() {
   return (
-    <div>
-      hello
-    </div>
+    <>
+    <Header />
+    <Hero />
+    <SkillsScroller />
+    <Projects />
+    <Footer />
+    </>
   );
 }
