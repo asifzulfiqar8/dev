dea3962a9221b2901c0f33b7d9a4df21ea6260cb
asifzulfiqar8 <asifzulfiqar43@gmail.com>
2025-05-18 20:39:01 +0500

    feat: dev mode on

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="section min-h-screen grid place-items-center">
-      <h2 className="text-3xl uppercase font-bold">Coming Soon</h2>
+      <h2 className="text-3xl uppercase font-bold">Dev Mode</h2>
     </div>
   );
 }
