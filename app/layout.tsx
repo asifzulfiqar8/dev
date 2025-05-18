15110db0d8b897b2b902faf900700aeaa36bf775
asifzulfiqar8 <asifzulfiqar43@gmail.com>
2025-05-19 01:15:14 +0500

    feat: boilerplate added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Asif Zulfiqar - Software Engineer",
   description:
@@ -39,8 +29,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "https://asifzulfiqar.vercel.app/assets/images/asif.png",
-        width: 1200,
-        height: 630,
+        width: 50,
+        height: 50,
         alt: "Asif Zulfiqar - Software Engineer",
       },
     ],
@@ -66,11 +56,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`antialiased`}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 export default function Home() {
   return (
-    <div className="section min-h-screen grid place-items-center">
-      <h2 className="text-3xl uppercase font-bold">Dev Mode</h2>
-    </div>
+    <>
+      <Header />
+      <Footer />
+    </>
   );
 }
